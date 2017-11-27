db748dbcd21a152273f1b0705e585a62b5b012f7
Andy Boyle <andy@sudostream.io>
2017-11-27 10:21:02 +0000

    script done on extracting schools from csv

--- schools/extractSchoolsFromCsv.scala
+++ schools/extractSchoolsFromCsv.scala
@@ -6,19 +6,19 @@ import java.nio.file.{Files, Paths}
 
 import scala.collection.mutable.ListBuffer
 
-var schoolName = ""
-var address = ""
-var postCode = ""
-var telephone = ""
-var localAuthority = ""
+var currentSchoolName = ""
+var currentAddress = ""
+var currentPostCode = ""
+var currentTelephone = ""
+var currentLocalAuthority = ""
 
 
 def blankTheCurrents(): Unit = {
-  schoolName = ""
-  address = ""
-  postCode = ""
-  telephone = ""
-  localAuthority = ""
+  currentSchoolName = ""
+  currentAddress = ""
+  currentPostCode = ""
+  currentTelephone = ""
+  currentLocalAuthority = ""
 }
 
 case class School(
@@ -38,65 +38,62 @@ val schoolsFile: String = args(0)
 val schoolsFilePath = Paths.get(schoolsFile)
 val schoolsFileArray = Files.readAllLines(schoolsFilePath)
 val schoolsFileArrayScala = schoolsFileArray.toArray.toList
-
-var processingASchool = false
-val people: ListBuffer[School] = new ListBuffer[School]()
-
+val schools: ListBuffer[School] = new ListBuffer[School]()
 
 for (line <- schoolsFileArrayScala) {
   val lineAsString = line.asInstanceOf[String]
-  if (processingASchool) {
-    if (lineAsString.contains("Surname")) {
-      val startPosition = lineAsString.indexOf("Surname: ") + 9
-      val endPosition = lineAsString.indexOf("</p>")
-      currentSurname = lineAsString.substring(startPosition, endPosition)
-    } else if (lineAsString.contains("Email")) {
-      val startPosition = lineAsString.indexOf("Email: ") + 7
-      val endPosition = lineAsString.indexOf("</p>")
-      currentEmail = lineAsString.substring(startPosition, endPosition)
-    } else if (lineAsString.contains("Local Authority")) {
-      val startPosition = lineAsString.indexOf("Local Authority: ") + 17
-      val endPosition = lineAsString.indexOf("</p>")
-      currentLocalAuthority = lineAsString.substring(startPosition, endPosition)
-    } else if (lineAsString.contains("Position Type")) {
-      val startPosition = lineAsString.indexOf("Position Type: ") + 15
-      val endPosition = lineAsString.indexOf("</p>")
-      currentPositionType = lineAsString.substring(startPosition, endPosition)
+  if (!lineAsString.contains("Address 1")) {
+    val csvElements = lineAsString.split(",")
+    // LA Name,School Name,Address 1,Address 2,Post code,Phone
+    currentLocalAuthority = csvElements(0).trim
+    currentSchoolName = csvElements(1).trim
+    currentAddress = csvElements(2).trim + ", " + csvElements(3).trim
+    currentPostCode = csvElements(4).trim
+    currentTelephone = csvElements(5).trim
 
-      val newPerson = Person(
-        firstname = currentFirstname,
-        surname = currentSurname,
-        email = currentEmail,
-        positionType = currentPositionType,
-        localAuthority = currentLocalAuthority
-      )
+    val newSchool = School(
+      name = currentSchoolName,
+      address = currentAddress,
+      postCode = currentPostCode,
+      telephone = currentTelephone,
+      localAuthority = currentLocalAuthority
+    )
 
-      people += newPerson
+    schools += newSchool
 
-      processingASchool = false
-      blankTheCurrents()
-    }
-  } else {
-    if (lineAsString.contains("Firstname")) {
-      processingASchool = true
-      val startPositionFirstName = lineAsString.indexOf("Firstname: ") + 11
-      val endPositionFirstName = lineAsString.indexOf("</p>")
-      val firstname = lineAsString.substring(startPositionFirstName, endPositionFirstName)
-      currentFirstname = firstname
-    }
+    blankTheCurrents()
   }
 }
 
-println(s"How many people: ${people.size}")
+println(s"How many schools: ${schools.size}")
 
-val charset = Charset.forName("US-ASCII")
-val outCsvFile = Paths.get(schoolsFile + ".csv")
+val charset = Charset.forName("UTF-8")
+val outCsvFile = Paths.get(schoolsFile + ".json")
 val writer = Files.newBufferedWriter(outCsvFile, charset)
 
-for ( person <- people) {
-  val lineToWrite =
-    s"${java.util.UUID.randomUUID().toString}, ${person.firstname}, ${person.surname}, ${person.positionType}, " +
-  s"${person.email}, ${person.localAuthority}\n"
+writer.write("[", 0, 1)
+var count = 0
+for (school <- schools) {
+  count = count + 1
+  val comma = if (count < schools.size) "," else ""
+
+  writeLine("  {")
+  writeLine(s"""    "_id" : "",""")
+  writeLine(s"""    "name" : "${school.name}",""")
+  writeLine(s"""    "address" : "${school.address}",""")
+  writeLine(s"""    "postCode" : "${school.postCode}",""")
+  writeLine(s"""    "telephone" : "${school.telephone}",""")
+  writeLine(s"""    "localAuthority" : "${school.telephone}",""")
+  writeLine(s"""    "country" : "SCOTLAND"""")
+  writeLine(s"""  }$comma""")
+}
+writer.write("]", 0, 1)
+
+def writeLine(stringToWrite: String ): Unit = {
+//  println("Writing line :" + stringToWrite)
+  val lineToWrite = stringToWrite + "\n"
   writer.write(lineToWrite, 0, lineToWrite.length)
 }
 
+writer.flush()
+writer.close()
